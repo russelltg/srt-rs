b3bb968246af0d7c2135b4f8ae6d5345c67ca140
Russell Greene <russellgreene8@gmail.com>
2018-02-28 14:26:02 -0700

    Finish Stream impl

--- src/bin.rs
+++ src/bin.rs
@@ -1,13 +1,16 @@
 extern crate bytes;
 extern crate srt;
+
+#[macro_use]
 extern crate futures;
+
 extern crate tokio;
 
 use bytes::{BytesMut, IntoBuf};
 
-use std::net::UdpSocket;
+use std::net::ToSocketAddrs;
 use srt::socket::{SrtSocketBuilder, SrtSocket};
-use std::io::{Cursor, Error};
+use std::io::{Error};
 
 use futures::prelude::*;
 
@@ -17,21 +20,28 @@ struct Peer {
     sock: SrtSocket,
 }
 
-// impl Future for Peer {
-//     type Item = ();
-//     type Error = Error;
+impl Future for Peer {
+    type Item = ();
+    type Error = Error;
 
-//     fn poll(&mut self) -> Poll<(), Error> {
-//         while let Async::Ready(packet) = self.sock.poll()? {
-//             println!("{:?}", packet);
-//         }
-//     }
-// }
+    fn poll(&mut self) -> Poll<(), Error> {
+        loop {
+            let packet = try_ready!(self.sock.poll());
+            println!("{:?}", packet);
+        }
+    }
+}
 
 fn main() {
-    //let peer = Peer{sock: SrtSocketBuilder::new("127.0.0.1:8171").build().unwrap()};
+    let b = BytesMut::with_capacity(65536);
+    let a: &mut [u8] = b.as_mut();
+    println!("{}", a.len());
 
-    // current_thread::run(|_| {
-    //     current_thread::spawn(peer);
-    // });
+    let peer = Peer{sock: SrtSocketBuilder::new("127.0.0.1:8171".to_socket_addrs().unwrap().next().unwrap()).build().unwrap()};
+
+    current_thread::run(|_| {
+        current_thread::spawn(peer.map_err(|e| {
+            eprintln!("Error received: {:?}", e);
+        }));
+    });
 }
--- src/socket.rs
+++ src/socket.rs
@@ -1,8 +1,8 @@
 use std::net::{SocketAddr, ToSocketAddrs};
-use std::io::{Error, ErrorKind, Result};
+use std::io::{Error, ErrorKind, Result, Cursor};
 use std::collections::VecDeque;
 
-use tokio::net::UdpSocket;
+use tokio::net::{UdpSocket, RecvDgram, SendDgram};
 
 use packet::Packet;
 
@@ -36,25 +36,22 @@ impl SrtSocketBuilder {
     pub fn build(self) -> Result<SrtSocket> {
 
         // start listening
-        let sock = UdpSocket::bind(self.local_addr.clone())?;
+        let sock = UdpSocket::bind(&self.local_addr)?;
 
         Ok(SrtSocket {
-            addr: self.local_addr,
             queue: VecDeque::new(),
-            future: SocketFuture::Recv(sock.recv_dgram(BytesMut::with_capacity(54436))),
+            future: SocketFuture::Recv(sock.recv_dgram(BytesMut::with_capacity(65536))),
         })
     }
 }
 
 pub enum SocketFuture {
-    Send(Box<Future<Item = (UdpSocket, T, usize, SocketAddr), Error=Error>>),
-    Recv(Box<Future<Item = (UdpSocket, BytesMut), Error=Error>>),
+    Send(SendDgram<BytesMut>),
+    Recv(RecvDgram<BytesMut>),
 }
 
 pub struct SrtSocket {
-    addr: SocketAddr,
-    queue: VecDeque<Packet>,
-
+    queue: VecDeque<(Packet, SocketAddr)>,
     future: SocketFuture,
 }
 
@@ -63,6 +60,49 @@ impl Stream for SrtSocket {
     type Error = Error;
 
     fn poll(&mut self) -> Poll<Option<Packet>, Error> {
-        
+
+        loop {
+
+            let mut pack = None;
+
+            let sock = match &mut self.future {
+                &mut SocketFuture::Recv(ref mut recv) => {
+                    let (sock, buff, n_bytes, from_addr) = try_ready!(recv.poll());
+
+                    // parse the packet
+                    pack = Some(Packet::parse(Cursor::new(buff.freeze().slice(0, n_bytes)))?);
+                    
+                    sock
+                }
+                &mut SocketFuture::Send(ref mut snd) => {
+                    let (sock, _) = try_ready!(snd.poll());
+                    sock
+                }
+            };
+
+            // start the next op
+            self.future = match self.queue.pop_front() {
+                Some((pack, addr)) => {
+                    // send the packet
+
+                    // serialize the packet
+                    let mut bytes = BytesMut::with_capacity(65536);
+                    pack.serialize(&mut bytes);
+
+                    // send it
+                    SocketFuture::Send(sock.send_dgram(bytes, &addr))
+                },
+                None => {
+                    // just receive
+                    let mut bytes = BytesMut::with_capacity(65536);
+
+                    SocketFuture::Recv(sock.recv_dgram(bytes))
+                }
+            };
+
+            if let Some(p) = pack {
+                return Ok(Async::Ready(Some(p)));
+            }
+        }
     }
 }
