43c10d50ff3947d74b356e2facc73a2dd1e76085
Russell Greene <russellgreene8@gmail.com>
2018-12-27 18:21:45 -0700

    Revamp lossy test, plus make it pass

--- tests/lossy.rs
+++ tests/lossy.rs
@@ -1,33 +1,31 @@
-use std::str;
 use std::thread;
 use std::time::{Duration, Instant};
 
 use bytes::Bytes;
-use failure::{format_err, Error};
+use failure::Error;
 use futures::{stream::iter_ok, Future, Sink, Stream};
 use futures_timer::Interval;
-use srt::{
-    ConnectionSettings, Receiver, Sender, SeqNumber, SocketID,
-    SrtCongestCtrl,
-};
+use srt::{ConnectionSettings, Receiver, Sender, SeqNumber, SocketID, SrtCongestCtrl};
 
 mod lossy_conn;
 use crate::lossy_conn::LossyConn;
 
 #[test]
-fn test_with_loss() {
+fn lossy() {
     let _ = env_logger::try_init();
 
-    const INIT_SEQ_NUM: u32 = 812_731;
+    const INIT_SEQ_NUM: u32 = 0;
     const ITERS: u32 = 10_000;
 
     // a stream of ascending stringified integers
-    let counting_stream = iter_ok(INIT_SEQ_NUM..(INIT_SEQ_NUM + ITERS))
+    let counting_stream = iter_ok(0..ITERS)
         .map(|i| Bytes::from(i.to_string()))
-        .zip(Interval::new(Duration::from_micros(100)))
+        .zip(Interval::new(Duration::from_millis(1)))
         .map(|(b, _)| b);
 
-    let (send, recv) = LossyConn::channel(0.05, Duration::from_secs(0), Duration::from_secs(0));
+    // 5% packet loss, 20ms delay
+    let (send, recv) =
+        LossyConn::channel(0.05, Duration::from_millis(20), Duration::from_millis(4));
 
     let sender = Sender::new(
         send,
@@ -40,7 +38,7 @@ fn test_with_loss() {
             max_packet_size: 1316,
             max_flow_size: 50_000,
             remote: "0.0.0.0:0".parse().unwrap(), // doesn't matter, it's getting discarded
-            tsbpd_latency: Duration::from_secs(100),
+            tsbpd_latency: Duration::from_secs(8),
         },
     );
 
@@ -54,11 +52,11 @@ fn test_with_loss() {
             max_packet_size: 1316,
             max_flow_size: 50_000,
             remote: "0.0.0.0:0".parse().unwrap(),
-            tsbpd_latency: Duration::from_secs(100),
+            tsbpd_latency: Duration::from_secs(8),
         },
     );
 
-    let t1 = thread::spawn(|| {
+    let sender = thread::spawn(|| {
         sender
             .send_all(counting_stream.map(|b| (Instant::now(), b)))
             .map_err(|e: Error| panic!("{:?}", e))
@@ -66,20 +64,28 @@ fn test_with_loss() {
             .unwrap();
     });
 
-    let t2 = thread::spawn(|| {
-        let mut next_data = INIT_SEQ_NUM;
+    let receiver = thread::spawn(|| {
+        let mut next_data = 0;
 
         for payload in recvr.wait() {
-            let (_, payload) = payload.unwrap();
+            let (ts, payload) = payload.unwrap();
 
-            assert_eq!(next_data.to_string(), str::from_utf8(&payload[..]).unwrap());
+            let diff = Instant::now() - ts;
+            let diff_ms = diff.as_secs() * 1_000 + u64::from(diff.subsec_nanos() / 1_000_000);
+
+            assert!(
+                7900 < diff_ms && diff_ms < 8700,
+                "Latency not in tolerance zone: {}ms",
+                diff_ms
+            );
+            assert_eq!(&next_data.to_string(), &payload);
 
             next_data += 1;
         }
 
-        assert_eq!(next_data, INIT_SEQ_NUM + ITERS);
+        assert_eq!(next_data, ITERS);
     });
 
-    t1.join().unwrap();
-    t2.join().unwrap();
+    sender.join().unwrap();
+    receiver.join().unwrap();
 }
