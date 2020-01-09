670d54e6fa2d0dcf830a6617a6a4ddd4f58197b4
Robert Ream <robertream@gmail.com>
2020-01-09 11:49:14 -0800

    refactor connect to mealy machine

--- src/pending_connection/connect.rs
+++ src/pending_connection/connect.rs
@@ -1,20 +1,217 @@
 use std::net::{IpAddr, SocketAddr};
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
 use failure::Error;
-
-use futures::prelude::*;
 use futures::select;
-
-use log::{debug, info, warn};
+use futures::prelude::*;
+use tokio::time::Instant;
 use tokio::time::interval;
 
-use crate::packet::{
-    ControlPacket, ControlTypes, HandshakeControlInfo, HandshakeVSInfo, Packet, ShakeType,
-    SocketType, SrtControlPacket, SrtHandshake, SrtShakeFlags,
-};
+use log::warn;
+
+use crate::packet::*;
 use crate::util::get_packet;
-use crate::{Connection, ConnectionSettings, SocketID, SrtVersion};
+use crate::{Connection, ConnectionSettings, DataPacket, SeqNumber, SocketID, SrtVersion};
+
+use ConnectError::*;
+use ConnectState::*;
+
+pub struct ConnectConfiguration {
+    remote: SocketAddr,
+    local_sockid: SocketID,
+    local_addr: IpAddr,
+    tsbpd_latency: Duration,
+}
+
+#[derive(Clone)]
+pub enum ConnectState {
+    Configured(SeqNumber),
+    InductionResponseWait(Packet),
+    ConclusionResponseWait(Packet),
+    Connected(ConnectionSettings),
+}
+
+impl ConnectState {
+    pub fn new() -> ConnectState {
+        Configured(rand::random())
+    }
+}
+
+#[derive(Debug)]
+pub enum ConnectError {
+    //#[error("Expected Control packet, expected: {0} found: {1}")]
+    ControlExpected(ShakeType, DataPacket),
+    //#[error("Expected Handshake packet, expected: {0} found: {1}")]
+    HandshakeExpected(ShakeType, ControlTypes),
+    //#[error("Expected Induction (1) packet, found: {0}")]
+    InductionExpected(HandshakeControlInfo),
+    //#[error("Expected packets from different host, expected: {0} found: {1}")]
+    UnexpectedHost(SocketAddr, SocketAddr),
+    //#[error("Expected Conclusion (-1) packet, found: {0}")]
+    ConclusionExpected(HandshakeControlInfo),
+    //#[error("Unsupported protocol version, expected: v5 found v{0}")]
+    UnsupportedProtocolVersion(u32),
+    // TODO: why don't we validate the cookie on responses
+    //#[error("Received invalid cookie handshake from [address], expected: {0} found {1}")]
+    //InvalidHandshakeCookie(i32, i32),
+    // TODO: why don't we validate we have an SRT response
+    //#[error("Expected SRT handshake request in conclusion handshake, found {0}")]
+    //SrtHandshakeExpected(HandshakeControlInfo),
+}
+
+pub struct Connect(ConnectConfiguration, ConnectState);
+
+pub type ConnectResult = Result<Option<(Packet, SocketAddr)>, ConnectError>;
+
+impl Connect {
+    fn on_start(&mut self, init_seq_num: SeqNumber) -> ConnectResult {
+        let config = &self.0;
+        let packet = Packet::Control(ControlPacket {
+            dest_sockid: SocketID(0),
+            timestamp: 0, // TODO: this is not zero in the reference implementation
+            control_type: ControlTypes::Handshake(HandshakeControlInfo {
+                init_seq_num,
+                max_packet_size: 1500, // TODO: take as a parameter
+                max_flow_size: 8192,   // TODO: take as a parameter
+                socket_id: config.local_sockid,
+                shake_type: ShakeType::Induction,
+                peer_addr: config.local_addr,
+                syn_cookie: 0,
+                info: HandshakeVSInfo::V4(SocketType::Datagram),
+            }),
+        });
+        self.1 = InductionResponseWait(packet.clone());
+        Ok(Some((packet, self.0.remote)))
+    }
+
+    pub fn wait_for_induction(
+        &mut self,
+        from: SocketAddr,
+        timestamp: i32,
+        info: HandshakeControlInfo,
+    ) -> ConnectResult {
+        let config = &self.0;
+        match (info.shake_type, info.info.version(), from) {
+            (ShakeType::Induction, 5, from) if from == config.remote => {
+                // send back a packet with the same syn cookie
+                let packet = Packet::Control(ControlPacket {
+                    timestamp,
+                    dest_sockid: SocketID(0),
+                    control_type: ControlTypes::Handshake(HandshakeControlInfo {
+                        shake_type: ShakeType::Conclusion,
+                        socket_id: config.local_sockid,
+                        info: HandshakeVSInfo::V5 {
+                            crypto_size: 0, // TODO: implement
+                            ext_hs: Some(SrtControlPacket::HandshakeRequest(SrtHandshake {
+                                version: SrtVersion::CURRENT,
+                                // TODO: this is hyper bad, don't blindly set send flag
+                                // if you don't pass TSBPDRCV, it doens't set the latency correctly for some reason. Requires more research
+                                peer_latency: Duration::from_secs(0), // TODO: research
+                                flags: SrtShakeFlags::TSBPDSND | SrtShakeFlags::TSBPDRCV, // TODO: the reference implementation sets a lot more of these, research
+                                latency: config.tsbpd_latency,
+                            })),
+                            ext_km: None,
+                            // ext_km: self.crypto.as_mut().map(|manager| {
+                            //     SrtControlPacket::KeyManagerRequest(SrtKeyMessage {
+                            //         pt: 2,       // TODO: what is this
+                            //         sign: 8_233, // TODO: again
+                            //         keki: 0,
+                            //         cipher: CipherType::CTR,
+                            //         auth: 0,
+                            //         se: 2,
+                            //         salt: Vec::from(manager.salt()),
+                            //         even_key: Some(manager.wrap_key().unwrap()),
+                            //         odd_key: None,
+                            //         wrap_data: [0; 8],
+                            //     })
+                            // }),
+                            ext_config: None,
+                        },
+                        ..info
+                    }),
+                });
+                self.1 = ConclusionResponseWait(packet.clone());
+                Ok(Some((packet, from)))
+            }
+            (ShakeType::Induction, 5, from) => Err(UnexpectedHost(config.remote, from)),
+            (ShakeType::Induction, version, _) => Err(UnsupportedProtocolVersion(version)),
+            (_, _, _) => Err(InductionExpected(info)),
+        }
+    }
+
+    fn wait_for_conclusion(
+        &mut self,
+        from: SocketAddr,
+        info: HandshakeControlInfo,
+    ) -> ConnectResult {
+        let config = &self.0;
+        match (info.shake_type, info.info.version(), from) {
+            (ShakeType::Conclusion, 5, from) if from == config.remote => {
+                let latency = match info.info {
+                    HandshakeVSInfo::V5 {
+                        ext_hs: Some(SrtControlPacket::HandshakeResponse(hs)),
+                        ..
+                    } => hs.latency,
+                    _ => {
+                        warn!("Did not get SRT handhsake in conclusion handshake packet, using latency from connector's end");
+                        config.tsbpd_latency
+                    }
+                };
+
+                self.1 = Connected(ConnectionSettings {
+                    remote: config.remote,
+                    max_flow_size: info.max_flow_size,
+                    max_packet_size: info.max_packet_size,
+                    init_seq_num: info.init_seq_num,
+                    // restamp the socket start time, so TSBPD works correctly.
+                    // TODO: technically it would be 1 rtt off....
+                    socket_start_time: Instant::now().into_std(),
+                    local_sockid: config.local_sockid,
+                    remote_sockid: info.socket_id,
+                    tsbpd_latency: latency,
+                });
+                // TODO: no handshake retransmit packet needed? is this right? Needs testing.
+
+                Ok(None)
+            }
+            (ShakeType::Conclusion, 5, from) => Err(UnexpectedHost(config.remote, from)),
+            (ShakeType::Conclusion, version, _) => Err(UnsupportedProtocolVersion(version)),
+            (ShakeType::Induction, _, _) => Ok(None),
+            (_, _, _) => Err(ConclusionExpected(info)),
+        }
+    }
+
+    pub fn next_packet(&mut self, next: (Packet, SocketAddr)) -> ConnectResult {
+        let (packet, from) = next;
+        match (self.1.clone(), packet) {
+            (InductionResponseWait(_), Packet::Control(control)) => {
+                match control.control_type {
+                    ControlTypes::Handshake(shake) =>
+                        self.wait_for_induction(from, control.timestamp,shake),
+                    control_type => Err(HandshakeExpected(ShakeType::Induction, control_type)),
+                }
+            },
+            (ConclusionResponseWait(_), Packet::Control(control)) => {
+                match control.control_type {
+                    ControlTypes::Handshake(shake) =>
+                        self.wait_for_conclusion(from, shake),
+                    control_type => Err(HandshakeExpected(ShakeType::Induction, control_type)),
+                }
+            },
+            (_, Packet::Data(data)) => Err(ControlExpected(ShakeType::Induction, data)),
+            (_, _) => Ok(None),
+        }
+    }
+
+    pub fn next_tick(&mut self, _now: Instant) -> ConnectResult {
+        match self.1.clone() {
+            Configured(init_seq_num) => self.on_start(init_seq_num),
+            InductionResponseWait(request_packet) => Ok(Some((request_packet, self.0.remote))),
+            ConclusionResponseWait(request_packet) => Ok(Some((request_packet, self.0.remote))),
+            _ => Ok(None),
+        }
+    }
+}
 
 pub async fn connect<T>(
     sock: &mut T,
@@ -29,180 +226,40 @@ where
         + Sink<(Packet, SocketAddr), Error = Error>
         + Unpin,
 {
-    info!("Got hanshake from {}", remote);
-    let mut send_interval = interval(Duration::from_millis(100));
+    let mut connect = Connect(
+        ConnectConfiguration {
+            remote,
+            local_sockid,
+            local_addr,
+            tsbpd_latency,
+        },
+        ConnectState::new(),
+    );
 
-    let (timestamp, hs_info) =
-        get_initial_handshake(sock, local_addr, local_sockid, remote).await?;
-
-    // send back a packet with the same syn cookie
-    let pack = Packet::Control(ControlPacket {
-        dest_sockid: SocketID(0),
-        timestamp,
-        control_type: ControlTypes::Handshake(HandshakeControlInfo {
-            shake_type: ShakeType::Conclusion,
-            socket_id: local_sockid,
-            info: HandshakeVSInfo::V5 {
-                crypto_size: 0, // TODO: implement
-                ext_hs: Some(SrtControlPacket::HandshakeRequest(SrtHandshake {
-                    version: SrtVersion::CURRENT,
-                    // TODO: this is hyper bad, don't blindly set send flag
-                    // if you don't pass TSBPDRCV, it doens't set the latency correctly for some reason. Requires more research
-                    peer_latency: Duration::from_secs(0), // TODO: research
-                    flags: SrtShakeFlags::TSBPDSND | SrtShakeFlags::TSBPDRCV, // TODO: the reference implementation sets a lot more of these, research
-                    latency: tsbpd_latency,
-                })),
-                ext_km: None,
-                // ext_km: self.crypto.as_mut().map(|manager| {
-                //     SrtControlPacket::KeyManagerRequest(SrtKeyMessage {
-                //         pt: 2,       // TODO: what is this
-                //         sign: 8_233, // TODO: again
-                //         keki: 0,
-                //         cipher: CipherType::CTR,
-                //         auth: 0,
-                //         se: 2,
-                //         salt: Vec::from(manager.salt()),
-                //         even_key: Some(manager.wrap_key().unwrap()),
-                //         odd_key: None,
-                //         wrap_data: [0; 8],
-                //     })
-                // }),
-                ext_config: None,
-            },
-            ..hs_info
-        }),
-    });
-
-    sock.send((pack.clone(), remote)).await?;
-
+    let mut tick_interval = interval(Duration::from_millis(100));
     loop {
-        let (packet, from) = select! {
-            _ = send_interval.tick().fuse() => {sock.send((pack.clone(), remote)).await?; continue},
-            res = get_packet(sock).fuse() => res?
+        let result = select! {
+            now = tick_interval.tick().fuse() => connect.next_tick(now),
+            packet = get_packet(sock).fuse() => connect.next_packet(packet?),
         };
-        if let Packet::Control(ControlPacket {
-            dest_sockid,
-            control_type:
-                ControlTypes::Handshake(
-                    info @ HandshakeControlInfo {
-                        shake_type: ShakeType::Conclusion,
-                        ..
-                    },
-                ),
-            ..
-        }) = packet
-        {
-            if from != remote {
-                warn!("Got packet from {}, expected {}", from, remote)
-            }
-            if dest_sockid != local_sockid {
-                warn!(
-                    "Unexpected destination socket id, expected {:?}, got {:?}",
-                    local_sockid, dest_sockid
-                );
-                continue;
-            }
-            let latency = if let HandshakeVSInfo::V5 {
-                ext_hs: Some(SrtControlPacket::HandshakeResponse(hs)),
-                ..
-            } = info.info
-            {
-                hs.latency
-            } else {
-                warn!("Did not get SRT handhsake in conclusion handshake packet, using latency from connector's end");
-                tsbpd_latency
-            };
 
-            info!(
-                "Got second handshake, connection established to {} with latency {:?}",
-                remote, latency
-            );
-            return Ok(Connection {
-                settings: ConnectionSettings {
-                    remote,
-                    max_flow_size: info.max_flow_size,
-                    max_packet_size: info.max_packet_size,
-                    init_seq_num: info.init_seq_num,
-                    socket_start_time: Instant::now(), // restamp the socket start time, so TSBPD works correctly. TODO: technically it would be 1 rtt off....
-                    local_sockid,
-                    remote_sockid: info.socket_id,
-                    tsbpd_latency: latency,
-                },
-                // TODO: is this right? Needs testing.
-                hs_returner: Box::new(move |_| None),
-            });
+        match result {
+            Ok(Some(packet)) => {
+                sock.send(packet).await?;
+            }
+            Err(e) => {
+                eprintln!("{:?}", e);
+            }
+            _ => {}
+        }
+        match connect.1.clone() {
+            Connected(settings) => {
+                return Ok(Connection {
+                    settings,
+                    hs_returner: Box::new(move |_| None),
+                });
+            }
+            _ => {}
         }
     }
-}
-
-async fn get_initial_handshake<T>(
-    sock: &mut T,
-    local_addr: IpAddr,
-    local_sockid: SocketID,
-    remote: SocketAddr,
-) -> Result<(i32, HandshakeControlInfo), Error>
-where
-    T: Stream<Item = Result<(Packet, SocketAddr), Error>>
-        + Sink<(Packet, SocketAddr), Error = Error>
-        + Unpin,
-{
-    let mut send_interval = interval(Duration::from_millis(100));
-
-    info!("Connecting to {}...", remote);
-
-    let request_packet = Packet::Control(ControlPacket {
-        dest_sockid: SocketID(0),
-        timestamp: 0, // TODO: this is not zero in the reference implementation
-        control_type: ControlTypes::Handshake(HandshakeControlInfo {
-            init_seq_num: rand::random(),
-            max_packet_size: 1500, // TODO: take as a parameter
-            max_flow_size: 8192,   // TODO: take as a parameter
-            socket_id: local_sockid,
-            shake_type: ShakeType::Induction,
-            peer_addr: local_addr,
-            syn_cookie: 0,
-            info: HandshakeVSInfo::V4(SocketType::Datagram),
-        }),
-    });
-
-    sock.send((request_packet.clone(), remote)).await?;
-
-    info!("Sent first packet to {}", remote);
-
-    // get first response packet
-    let (timestamp, hs_info) = loop {
-        // just drop the future that didn't finish first
-        let (packet, addr) = select! {
-            _ = send_interval.tick().fuse() => {sock.send((request_packet.clone(), remote)).await?; continue},
-            res = get_packet(sock).fuse() => res?
-        };
-
-        debug!("Got packet from {}", addr);
-        // make sure the socket id and packet type match
-        if let Packet::Control(ControlPacket {
-            timestamp,
-            control_type: ControlTypes::Handshake(info @ HandshakeControlInfo { .. }),
-            ..
-        }) = packet
-        {
-            if info.shake_type != ShakeType::Induction {
-                info!(
-                    "Expected Induction (1) packet, got {:?} ({})",
-                    info.shake_type, info.shake_type as u32
-                );
-                continue;
-            }
-            if addr != remote {
-                warn!("Expected packet from {}, got {}", remote, addr);
-                continue;
-            }
-            if info.info.version() != 5 {
-                warn!("Handshake was HSv4, expected HSv5");
-                continue;
-            }
-            break (timestamp, info);
-        }
-    };
-
-    Ok((timestamp, hs_info))
 }
