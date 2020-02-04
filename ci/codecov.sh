fb4d5859e41b7ec937a8d74fd0cebcef9d8cf901
Russell Greene <russellgreene8@gmail.com>
2020-02-03 21:54:21 -0700

    try using the deps directory instead

--- ci/codecov.sh
+++ ci/codecov.sh
@@ -14,8 +14,11 @@ make install
 export PATH=~/.local/bin:$PATH
 cd ../..
 rm -rf kcov-master master.tar.gz
-for file in target/debug/{bidirectional,latency_exchange,lossy_connect,lossy,message_splitting,multiplexer,not_enough_latency,single_packet_tsbpd,srt,stransmit_cmdline}-*[^\.d]
-    do mkdir -p "target/cov/$(basename $file)"; kcov --exclude-pattern=/.cargo,/usr/lib,/usr/include,/lib,tests/ --verify "target/cov/$(basename $file)" "$file"
+ls target/debug
+ls target/debug/deps
+for file in target/debug/deps/{bidirectional,latency_exchange,lossy_connect,lossy,message_splitting,multiplexer,not_enough_latency,single_packet_tsbpd,srt,stransmit_cmdline}-*[^\.d]
+    do mkdir -p "target/cov/$(basename $file)"
+    kcov --exclude-pattern=/.cargo,/usr/lib,/usr/include,/lib,tests/ --verify "target/cov/$(basename $file)" "$file"
 done
 bash <(curl -s https://codecov.io/bash)
 echo "Uploaded code coverage"
