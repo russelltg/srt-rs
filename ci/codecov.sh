37eb032c744d3c47f734bc27a5e02f1d7d74b360
Russell Greene <russellgreene8@gmail.com>
2020-02-03 21:38:00 -0700

    add more exclusion directories

--- ci/codecov.sh
+++ ci/codecov.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-sudo apt-get update
-sudo apt-get install libbfd-dev libdw-dev libelf-dev libcurl4-openssl-dev
+#sudo apt-get update
+#sudo apt-get install libbfd-dev libdw-dev libelf-dev libcurl4-openssl-dev libbfd-dev libiberty-dev
 wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
 tar xzf master.tar.gz
 cd kcov-master
@@ -15,7 +15,7 @@ export PATH=~/.local/bin:$PATH
 cd ../..
 rm -rf kcov-master master.tar.gz
 for file in target/debug/{bidirectional,latency_exchange,lossy_connect,lossy,message_splitting,multiplexer,not_enough_latency,single_packet_tsbpd,srt,stransmit_cmdline}-*[^\.d]
-    do mkdir -p "target/cov/$(basename $file)"; kcov --exclude-pattern=/.cargo,/usr/lib,tests/ --verify "target/cov/$(basename $file)" "$file"
+    do mkdir -p "target/cov/$(basename $file)"; kcov --exclude-pattern=/.cargo,/usr/lib,/usr/include,/lib,tests/ --verify "target/cov/$(basename $file)" "$file"
 done
 bash <(curl -s https://codecov.io/bash)
 echo "Uploaded code coverage"
