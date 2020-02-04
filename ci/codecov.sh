f1d1f997e771b7f64b8a75f91249972163ab1747
Russell Greene <russellgreene8@gmail.com>
2020-02-03 20:50:50 -0700

    fix misspelled package

--- ci/codecov.sh
+++ ci/codecov.sh
@@ -2,7 +2,7 @@
 set -ex
 
 sudo apt-get update
-sudo apt-get install libbfd-dev libdw-dev libelv-dev
+sudo apt-get install libbfd-dev libdw-dev libelf-dev
 wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
 tar xzf master.tar.gz
 cd kcov-master
