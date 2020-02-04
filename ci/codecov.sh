85759544aa4437a577be8b739c7ef0552657ffa4
Russell Greene <russellgreene8@gmail.com>
2020-02-03 20:36:01 -0700

    fix path variable in codecov

--- ci/codecov.sh
+++ ci/codecov.sh
@@ -9,6 +9,7 @@ cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=~/.local
 make
 make install
+export PATH=~/.local/bin:$PATH
 cd ../..
 rm -rf kcov-master master.tar.gz
 for file in target/debug/{bidirectional,latency_exchange,lossy_connect,lossy,message_splitting,multiplexer,not_enough_latency,single_packet_tsbpd,srt,stransmit_cmdline}-*[^\.d]
