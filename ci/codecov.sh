2bdaaad9c1aae1cadc4a5b90703e70099b541aba
Russell Greene <russellgreene8@gmail.com>
2020-02-09 10:53:49 -0700

    simplify the kcov script so hopefully it works

--- ci/codecov.sh
+++ ci/codecov.sh
@@ -11,16 +11,17 @@ cd kcov-master
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=~/.local
-make
+make -j`nproc`
 make install
 export PATH=~/.local/bin:$PATH
 cd ../..
 rm -rf kcov-master master.tar.gz
-ls target/debug
-ls target/debug/deps
-for file in target/debug/deps/{bidirectional,latency_exchange,lossy_connect,lossy,message_splitting,multiplexer,not_enough_latency,single_packet_tsbpd,srt,stransmit_cmdline}-*[^\.d]
-    do mkdir -p "target/cov/$(basename $file)"
-    kcov --exclude-pattern=/.cargo,/usr/lib,/usr/include,/lib,tests/ --verify "target/cov/$(basename $file)" "$file"
+for file in target/debug/deps/{bidirectional,latency_exchange,lossy_connect,lossy,message_splitting,multiplexer,not_enough_latency,single_packet_tsbpd,srt,stransmit_cmdline}-*; do
+    if [[ -x "$file" ]]; then
+        echo Doing kcov for $file
+        mkdir -p "target/cov/$(basename $file)"
+        kcov --exclude-pattern=/.cargo,/usr/lib,/usr/include,/lib,tests/ --verify "target/cov/$(basename $file)" "$file"
+    fi
 done
 bash <(curl -s https://codecov.io/bash)
 echo "Uploaded code coverage"
