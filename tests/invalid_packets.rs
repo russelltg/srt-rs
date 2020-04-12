d3175b97895fc8880be1d2682f0ea61104cff97b
Russell Greene <build@churchillnavigation.com>
2020-04-12 11:54:36 -0600

    format new test

--- tests/invalid_packets.rs
+++ tests/invalid_packets.rs
@@ -1,30 +1,36 @@
-
-use srt::SrtSocketBuilder;
-use futures::stream;
-use futures::prelude::*;
-use tokio::time::interval;
-use std::time::{Instant, Duration};
 use bytes::Bytes;
-use tokio::net::UdpSocket;
+use futures::prelude::*;
+use futures::stream;
 use rand::Rng;
-
+use srt::SrtSocketBuilder;
+use std::time::{Duration, Instant};
+use tokio::net::UdpSocket;
+use tokio::time::interval;
 
 // Send a bunch of invalid packets to the socket, making sure that it can handle it
 #[tokio::test]
 async fn invalid_packets() {
     let sender = async {
-        let mut sender = SrtSocketBuilder::new_connect("127.0.0.1:8876").local_port(8877).connect().await.unwrap();
-        
+        let mut sender = SrtSocketBuilder::new_connect("127.0.0.1:8876")
+            .local_port(8877)
+            .connect()
+            .await
+            .unwrap();
 
         let mut counting_stream = stream::iter(0..100)
             .zip(interval(Duration::from_millis(1)))
-            .map(|(i, _)| Bytes::from(i.to_string())).map(|b| Ok((Instant::now(), b)));
+            .map(|(i, _)| Bytes::from(i.to_string()))
+            .map(|b| Ok((Instant::now(), b)));
 
         sender.send_all(&mut counting_stream).await.unwrap();
         sender.close().await.unwrap();
     };
     let recvr = async {
-        let mut recvr = SrtSocketBuilder::new_listen().local_port(8876).connect().await.unwrap();
+        let mut recvr = SrtSocketBuilder::new_listen()
+            .local_port(8876)
+            .connect()
+            .await
+            .unwrap();
 
         for _ in 0..100 {
             recvr.try_next().await.unwrap().unwrap();
@@ -36,7 +42,7 @@ async fn invalid_packets() {
     let garbage = async {
         let mut sock = UdpSocket::bind(&"127.0.0.1:0").await.unwrap();
         loop {
-            let mut rng = rand::thread_rng();            
+            let mut rng = rand::thread_rng();
             let mut to_send = vec![0; rng.gen_range(1, 1024)];
             for i in &mut to_send {
                 *i = rng.gen();
@@ -52,4 +58,3 @@ async fn invalid_packets() {
         _ = garbage.fuse() => {}
     }
 }
-
