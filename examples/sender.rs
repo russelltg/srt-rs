7eb68257525804199c94ec5835795aaad015c3fa
Marc-Antoine Arnaud <maarnaud@media-io.com>
2020-05-04 17:59:40 +0200

    fix identation

--- examples/sender.rs
+++ examples/sender.rs
@@ -7,20 +7,20 @@ use std::{thread, time};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-  let mut srt_socket = SrtSocketBuilder::new_listen()
-    .local_port(3333)
-    .connect()
-    .await?;
-  let mut count = 0;
+    let mut srt_socket = SrtSocketBuilder::new_listen()
+        .local_port(3333)
+        .connect()
+        .await?;
+    let mut count = 0;
 
-  loop {
-    srt_socket
-      .send((Instant::now(), Bytes::from(vec![0; 1000])))
-      .await?;
-    print!("\rSended {:?} packets", count);
-    count += 1;
+    loop {
+        srt_socket
+            .send((Instant::now(), Bytes::from(vec![0; 8000])))
+            .await?;
+        print!("\rSended {:?} packets", count);
+        count += 1;
 
-    let ten_millis = time::Duration::from_millis(10);
-    thread::sleep(ten_millis);
-  }
+        let ten_millis = time::Duration::from_millis(10);
+        thread::sleep(ten_millis);
+    }
 }
