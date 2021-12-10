e171b2b7db006e62d7f7eeaaeb1a0eaf812dd091
Robert Ream <robertream@gmail.com>
2021-12-10 11:33:55 -0800

    further derive_more

--- srt-protocol/src/options/address.rs
+++ srt-protocol/src/options/address.rs
@@ -1,13 +1,14 @@
 use std::{
     convert::TryFrom,
     net::{AddrParseError, SocketAddr},
-    ops::Deref,
 };
 
+use derive_more::*;
+
 // A simple wrapper for SocketAddr. This is used to support scenarios where using ToSocketAddrs in
-// function parameters is undesirable or in possible, yet similar ergonomic static string
-// configuration use for addresses is desired.
-#[derive(Clone, Debug, Eq, PartialEq)]
+// function parameters is undesirable or impossible, yet still support similar use of ergonomic
+// static string configuration for addresses when desired.
+#[derive(Clone, Debug, Deref, Into, From, Eq, PartialEq)]
 pub struct SocketAddress(SocketAddr);
 
 impl TryFrom<&str> for SocketAddress {
@@ -20,25 +21,5 @@ impl TryFrom<&str> for SocketAddress {
             address.parse()?
         };
         Ok(Self(address))
-    }
-}
-
-impl From<SocketAddr> for SocketAddress {
-    fn from(address: SocketAddr) -> Self {
-        Self(address)
-    }
-}
-
-impl From<SocketAddress> for SocketAddr {
-    fn from(address: SocketAddress) -> Self {
-        address.0
-    }
-}
-
-impl Deref for SocketAddress {
-    type Target = SocketAddr;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
     }
 }
