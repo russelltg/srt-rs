d3c9555ec43bbe56291efc22c9f680318594f376
Russell Greene <russellgreene8@gmail.com>
2023-06-20 00:53:12 -0500

    add missing field

--- srt-protocol/src/protocol/pending_connection/connect.rs
+++ srt-protocol/src/protocol/pending_connection/connect.rs
@@ -301,6 +301,7 @@ mod test {
                 send_buffer_size: options::PacketCount(8192),
                 max_packet_size: options::PacketSize(1500),
                 max_flow_size: options::PacketCount(8192),
+                peer_idle_timeout: Duration::from_secs(5),
             },
             sid,
             random(),
